c3638a4abd2abd66cad4be3db8ccf95a1f1a1100
Denis <Dysprosium92@ya.ru>
2022-03-28 21:24:50 +0300

    Change game engine
    Game engine got more functional style

--- src/Engine.hs
+++ src/Engine.hs
@@ -1,27 +1,43 @@
-module Engine (runGame) where
+module Engine
+(
+    main
+) where
 
+import Utils
 import Task
 import Types
-import Utils
+import Even
 
-gameDescription :: String
-gameDescription = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
+countRound :: Int
+countRound = 3
 
-runGame :: IO ()
-runGame = do
-  putStrLn "Welcome to the Brain Games!"
-  putStrLn "May I have your name? "
-  name <- getLine
-  putStrLn $ helloPerson name
-  putStrLn gameDescription
+main:: IO()
+main = do
+    name <- greetings
+    putStrLn Even.getDescroption
+    run name countRound
 
-  task <- createTask
-  let question = getQuestion task
-  let rightAnswer = getRightAnswer task
+greetings :: IO(Types.UserName)
+greetings = do
+    putStrLn "Welcome to the Brain Games!" 
+    putStrLn "May I have your name? "
+    name <- getLine
+    putStrLn $ Utils.helloPerson name
+    return name
 
-  putStrLn $ askQuestion question
-  putStr "Your answer: "
-  userAnswer <- getLine
-  if userAnswer == rightAnswer
-    then putStrLn "Correct!"
-    else putStrLn (makeErrorMessage userAnswer rightAnswer) >> putStrLn (makeEndPhrase name)
+run :: Types.UserName -> Int -> IO()
+run name countOfRound = do
+    if countOfRound == 0
+    then putStrLn (Utils.makeCongratulationsMessage name)
+    else do
+        task <- Even.genTask
+        putStrLn $ askQuestion $ Task.getQuestion task
+        userAnswer <- respond $ getQuestion task
+        if    userAnswer == Task.getRightAnswer task
+        then  putStrLn "Correct! " >> run name (countOfRound - 1)
+        else  putStrLn (Utils.makeErrorMessage userAnswer (Task.getRightAnswer task)) >> putStrLn (Utils.makeEndMessage name)
+
+respond :: String -> IO(String)
+respond task = do
+    putStrLn task
+    putStr "Your answer: " >> getLine
