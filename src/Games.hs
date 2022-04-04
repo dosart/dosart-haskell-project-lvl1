df7f351a72ee0d310a02640082597b2fa39febf8
Denis <Dysprosium92@ya.ru>
2022-04-04 20:33:42 +0300

    Add askGame function

--- src/Games.hs
+++ src/Games.hs
@@ -1,16 +1,19 @@
 module Games
-(
-
-) where
+  (
+  )
+where
 
 data Game = Even | Calc | SimpleNumber
 
 instance Show Game where
-    show  Even = "even."
-    show  Calc = "calc"
-    show  SimpleNumber = "simpleNumber"
+  show Even = "even."
+  show Calc = "calc"
+  show SimpleNumber = "simpleNumber"
+
+askGame :: IO String
+askGame = putStr "What game do you want to play? " >> getLine
 
 descroption :: Game -> String
-descroption  Even = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
-descroption  Calc = "What is the result of the expression?"
-descroption  SimpleNumber = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
+descroption Even = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
+descroption Calc = "What is the result of the expression?"
+descroption SimpleNumber = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
