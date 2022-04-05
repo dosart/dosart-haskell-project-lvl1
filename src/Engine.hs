ce914886006e6d88090d83763082a6cc084f797c
Denis <Dysprosium92@ya.ru>
2022-04-05 21:10:18 +0300

    Add functions genGame and gameName
    Function main rewrite with use these functions

--- src/Engine.hs
+++ src/Engine.hs
@@ -4,18 +4,21 @@ module Engine
 where
 
 import Even (genTask, getDescroption)
+import Games (Game, descroption, genGameBy)
 import Task (getQuestion, getRightAnswer)
-import Types (UserName, CountRound)
-import Utils (helloPerson, makeCongratulationsMessage, makeEndMessage, makeErrorMessage, askQuestion)
+import Types (CountRound, UserInput, UserName)
+import Utils (askQuestion, helloPerson, makeCongratulationsMessage, makeEndMessage, makeErrorMessage)
 
 countRound :: CountRound
 countRound = 3
 
 main :: IO ()
-main = do
-  name <- greetings
-  putStrLn getDescroption
-  run name countRound
+main = do 
+  user_name <- greetings
+  maybe_game <- genGame
+  case maybe_game of
+    Just game -> do print (descroption game)
+    Nothing -> print "Nothing"
 
 greetings :: IO UserName
 greetings = do
@@ -25,19 +28,25 @@ greetings = do
   putStrLn $ helloPerson name
   return name
 
+genGame :: IO (Maybe Game)
+genGame = fmap genGameBy gameName
+
+gameName :: IO String
+gameName = putStrLn "What game do you want to play? " >> getLine
+
 run :: UserName -> CountRound -> IO ()
-run name countOfRound = do
-  if countOfRound == 0
+run name count_round = do
+  if count_round == 0
     then putStrLn (makeCongratulationsMessage name)
     else do
       task <- genTask
       putStrLn $ askQuestion $ getQuestion task
       userAnswer <- respond $ getQuestion task
       if userAnswer == getRightAnswer task
-        then putStrLn "Correct! " >> run name (countOfRound - 1)
+        then putStrLn "Correct! " >> run name (count_round - 1)
         else putStrLn (makeErrorMessage userAnswer (getRightAnswer task)) >> putStrLn (makeEndMessage name)
 
 respond :: String -> IO String
 respond task = do
   putStrLn task
-  putStr "Your answer: " >> getLine
+  putStrLn "Your answer: " >> getLine
