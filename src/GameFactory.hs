67089cc7d50751127011d2b7720648894d8c51b3
Denis <Dysprosium92@ya.ru>
2022-04-12 20:13:31 +0300

    Fix string import

--- src/GameFactory.hs
+++ src/GameFactory.hs
@@ -1,14 +1,13 @@
 module GameFactory
   ( genGameBy,
-    Game,
   )
 where
 
 import Calc (genCalcGame)
+import Data.Char
 import Even (genEvenGame)
 import Game (Game)
 import Types (UserInput)
-import Data.Char
 
 genGameBy :: UserInput -> Maybe Game
 genGameBy = getGame . normalize
@@ -23,7 +22,7 @@ instance Show GameType where
   show Calc = "calc"
 
 getGame :: UserInput -> Maybe Game
-getGame user_input = lookup user_input $  map ( \(t, game) -> (show t, game) ) games
+getGame user_input = lookup user_input $ map (\(t, game) -> (show t, game)) games
 
 games :: [(GameType, Game)]
 games = [(Even, genEvenGame), (Calc, genCalcGame)]
