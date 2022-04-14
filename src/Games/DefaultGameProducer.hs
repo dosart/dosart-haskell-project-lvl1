15e37aecd780c3edf8582970d5ec06d20c452424
Denis <Dysprosium92@ya.ru>
2022-04-14 22:46:59 +0300

    Add DefaultGameProducer
    Module generate games with standard settings

---
+++ src/Games/DefaultGameProducer.hs
@@ -0,0 +1,26 @@
+module Games.DefaultGameProducer
+  ( genDefaultTask,
+  )
+where
+
+import Task (Task, produceTask)
+import Types (Question, RightAnswer)
+import RandomValue (genRandomNumber)
+
+genDefaultTask :: (Integer -> Bool) -> IO (Task Question RightAnswer)
+genDefaultTask pred = do
+  let f = genRightAnswer pred
+  let min_value = 0
+  let max_value = 100
+
+  fromRundom f min_value max_value
+
+fromRundom :: (Integer -> RightAnswer) -> Integer -> Integer -> IO (Task Question RightAnswer)
+fromRundom makeRightAnswer min_value max_value = do
+  number <- genRandomNumber min_value max_value
+  return (produceTask show makeRightAnswer number)
+
+genRightAnswer = rightAnswer
+
+rightAnswer :: (Integer -> Bool) -> Integer -> RightAnswer
+rightAnswer f number = if f number then "yes" else "no"
