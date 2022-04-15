b63f37ce0e55a5ddb25448437e3d2e020a397766
Denis <Dysprosium92@ya.ru>
2022-04-15 07:12:32 +0300

    Change module Games.Even
    Module uses genDefaultTask function from DefaultGameProducer module

--- src/Games/Even.hs
+++ src/Games/Even.hs
@@ -3,10 +3,10 @@ module Games.Even
   )
 where
 
+import Games.DefaultGameProducer (genDefaultTask)
 import Games.Game (Game (..))
-import RandomValue (genRandomNumber)
-import Task (Task, makeTask)
-import Types (RightAnswer)
+import Task (Task)
+import Types (Question, RightAnswer)
 
 genEvenGame :: Game
 genEvenGame = Game {description = describe, genTask = genEvenTask}
@@ -14,19 +14,5 @@ genEvenGame = Game {description = describe, genTask = genEvenTask}
 describe :: String
 describe = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
-genEvenTask :: IO Task
-genEvenTask = do
-  number <- genRandomNumber minValue maxValue
-  return (makeEvenTask number)
-
-minValue :: Integer
-minValue = 0
-
-maxValue :: Integer
-maxValue = 100
-
-makeEvenTask :: Integer -> Task
-makeEvenTask number = makeTask (show number) (convertToRightAnswer number)
-
-convertToRightAnswer :: Integer -> RightAnswer
-convertToRightAnswer number = if even number then "yes" else "no"
+genEvenTask :: IO (Task Question RightAnswer)
+genEvenTask = genDefaultTask even
