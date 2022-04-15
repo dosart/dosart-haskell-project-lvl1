a61ede7ee43b9018ddb23a96e0867a968e4b32e5
Denis <Dysprosium92@ya.ru>
2022-04-15 07:19:42 +0300

    Add prime number game
    This game uses  module prime from Data.Numbers

--- src/Games/Prime.hs
+++ src/Games/Prime.hs
@@ -3,14 +3,14 @@ module Games.Prime
   )
 where
 
+import Data.Numbers.Primes (isPrime)
 import Games.DefaultGameProducer (genDefaultTask)
 import Games.Game (Game (..))
 import Task (Task)
-import Types (RightAnswer, Question)
-import Data.Numbers.Primes (isPrime)
+import Types (Question, RightAnswer)
 
-genPrimeGame:: Game
-genPrimeGame= Game {description = describe, genTask = genPrimeTask}
+genPrimeGame :: Game
+genPrimeGame = Game {description = describe, genTask = genPrimeTask}
 
 describe :: String
 describe = "Answer \"yes\" if the number is prime, otherwise answer \"no\"."
