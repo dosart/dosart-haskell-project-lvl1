89a69ab7232f96d32d9f4c0be0e0b2f036d935c3
Denis <Dysprosium92@ya.ru>
2022-04-16 17:29:59 +0300

    Return implementation of game "Even number"
    Previous implementation reads worse and not testable

--- src/Games/Even.hs
+++ src/Games/Even.hs
@@ -14,5 +14,19 @@ genEvenGame = Game {description = describe, genTask = genEvenTask}
 describe :: String
 describe = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
-genEvenTask :: IO (Task Question RightAnswer)
-genEvenTask = genDefaultTask even
+genEvenTask :: IO Task
+genEvenTask = do
+  number <- genRandomNumber minValue maxValue
+  return (makeEvenTask number)
+
+minValue :: Integer
+minValue = 0
+
+maxValue :: Integer
+maxValue = 100
+
+makeEvenTask :: Integer -> Task
+makeEvenTask number = makeTask (show number) (convertToRightAnswer number)
+
+convertToRightAnswer :: Integer -> RightAnswer
+convertToRightAnswer number = if even number then "yes" else "no"
