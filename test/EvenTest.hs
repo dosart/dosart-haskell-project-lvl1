1d97edb3b7879083c2d4b049504a23e77356c91f
Denis <Dysprosium92@ya.ru>
2022-04-20 21:04:44 +0300

    Add tests for function makeGCDTask

--- test/EvenTest.hs
+++ test/EvenTest.hs
@@ -1,11 +1,14 @@
 module EvenTest
   ( prop_makeEvenTask,
+    prop_makeGCDTask,
   )
 where
 
 import Games.Even (makeEvenTask)
+import Games.GCD (makeGCDTask)
 import Task (getQuestion, getRightAnswer)
 import Test.QuickCheck
+import Types (Question, RightAnswer)
 
 prop_makeEvenTask number =
   if even number
@@ -15,3 +18,12 @@ prop_makeEvenTask number =
     task = makeEvenTask number
     question = getQuestion task
     righ_answer = getRightAnswer task
+
+
+prop_makeGCDTask num1 num2 = question == right_question && answer == righ_answer
+  where
+    task = makeGCDTask num1 num2
+    question = getQuestion task
+    answer = getRightAnswer task
+    right_question = show num1 ++ " " ++ show num2
+    righ_answer = show $ gcd num1 num2
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,8 @@
+import EvenTest (prop_makeEvenTask, prop_makeGCDTask)
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_makeEvenTask
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_makeGCDTask
+  putStrLn "Done!"
